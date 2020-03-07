f19f9c441427ce4a13c25139b59f58b6a6fdd9c8
zaCade <zaCade@hotmail.com>
2020-03-07 22:36:30 +0100

    Add a security nil check.

--- FixTransmogOutfits.lua
+++ FixTransmogOutfits.lua
@@ -23,10 +23,13 @@ local OriginalSaveOutfit = OriginalSaveOutfit or C_TransmogCollection.SaveOutfit
 
 -- Overwrite the function.
 function C_TransmogCollection.SaveOutfit(name, outfit, enchantOne, enchantTwo, icon)
-	-- Are we saving something to slot 16 (main hand) and 17 (off hand)?
-	-- If we are, check what we are actually changing to because for some odd reason it changes legion artifact skins as 0.
-	if outfit[16] then outfit[16] = GetSlotVisualSourceID(16, LE_TRANSMOG_TYPE_APPEARANCE) end
-	if outfit[17] then outfit[17] = GetSlotVisualSourceID(17, LE_TRANSMOG_TYPE_APPEARANCE) end
+	-- Checking for nil, just to make sure.
+	if outfit then
+		-- Are we saving something to slot 16 (main hand) and 17 (off hand)?
+		-- If we are, check what we are actually changing to because for some odd reason it changes legion artifact skins as 0.
+		if outfit[16] then outfit[16] = GetSlotVisualSourceID(16, LE_TRANSMOG_TYPE_APPEARANCE) end
+		if outfit[17] then outfit[17] = GetSlotVisualSourceID(17, LE_TRANSMOG_TYPE_APPEARANCE) end
+	end
 
 	-- Call the original function.
 	OriginalSaveOutfit(name, outfit, enchantOne, enchantTwo, icon);
