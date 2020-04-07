7dc4b1324484cf7aca867bc6eea13c98f58254bd
zaCade <zaCade@hotmail.com>
2020-04-07 12:25:31 +0200

    Change approach entirely, remove IsSourceArtifact().

--- FixTransmogOutfits.lua
+++ FixTransmogOutfits.lua
@@ -1,61 +1,83 @@
-if not C_Transmog or not C_TransmogCollection then return end
+if not WardrobeOutfitDropDownMixin then return end
 
--- Function to retrieve the actual visualSourceID for a slot.
-local function GetSlotVisualSourceID(slot, transmogType)
-	local visualSourceID;
-	local baseSourceID, _, appliedSourceID, _, pendingSourceID, _, hasPendingUndo = C_Transmog.GetSlotVisualInfo(slot, transmogType);
+-- Since original functions use a function 'IsSourceArtifact()' that is local to WardrobeOutfits.lua, we have to overwrite them.
+-- Therefor the code below are exact duplicates of the original blizzard code, just without the function mentioned above.
 
-	if hasPendingUndo then
-		visualSourceID = baseSourceID;
-	elseif pendingSourceID ~= 0 then
-		visualSourceID = pendingSourceID;
-	elseif appliedSourceID ~= 0 then
-		visualSourceID = appliedSourceID;
-	else
-		visualSourceID = baseSourceID;
+-- WardrobeOutfits.lua @ #90
+function WardrobeOutfitDropDownMixin:IsOutfitDressed()
+	if ( not self.selectedOutfitID ) then
+		return true;
+	end
+	local appearanceSources, mainHandEnchant, offHandEnchant = C_TransmogCollection.GetOutfitSources(self.selectedOutfitID);
+	if ( not appearanceSources ) then
+		return true;
 	end
 
-	return visualSourceID;
-end
-
--- Store the original set function.
-local OriginalSetPending = OriginalSetPending or C_Transmog.SetPending;
-
--- Overwrite the function.
-function C_Transmog.SetPending(slotID, transmogType, visualSourceID)
-	-- Since for example selecting a different legion artifact skin doesn't actually unlock the save button,
-	-- we'll have to tell it there has been an update, so it unlocks.
-
-	-- Check if the outfit dropdown exists.
-	if WardrobeTransmogFrame and WardrobeTransmogFrame.OutfitDropDown then
-		-- It exists, tell it to update the save button.
-		WardrobeTransmogFrame.OutfitDropDown:UpdateSaveButton();
+	for i = 1, #TRANSMOG_SLOTS do
+		if ( TRANSMOG_SLOTS[i].transmogType == LE_TRANSMOG_TYPE_APPEARANCE ) then
+			local sourceID = self:GetSlotSourceID(TRANSMOG_SLOTS[i].slot, LE_TRANSMOG_TYPE_APPEARANCE);
+			local slotID = GetInventorySlotInfo(TRANSMOG_SLOTS[i].slot);
+			if ( sourceID ~= NO_TRANSMOG_SOURCE_ID and sourceID ~= appearanceSources[slotID] ) then
+				if ( appearanceSources[slotID] ~= NO_TRANSMOG_SOURCE_ID ) then
+					return false;
+				end
+			end
+		end
 	end
-
-	-- Call the original function.
-	OriginalSetPending(slotID, transmogType, visualSourceID);
+	local mainHandSourceID = self:GetSlotSourceID("MAINHANDSLOT", LE_TRANSMOG_TYPE_ILLUSION);
+	if ( mainHandSourceID ~= mainHandEnchant ) then
+		return false;
+	end
+	local offHandSourceID = self:GetSlotSourceID("SECONDARYHANDSLOT", LE_TRANSMOG_TYPE_ILLUSION);
+	if ( offHandSourceID ~= offHandEnchant ) then
+		return false;
+	end
+	return true;
 end
 
--- Store the original save function.
-local OriginalSaveOutfit = OriginalSaveOutfit or C_TransmogCollection.SaveOutfit;
+-- WardrobeOutfits.lua @ #122
+function WardrobeOutfitDropDownMixin:CheckOutfitForSave(name)
+	local sources = { };
+	local mainHandEnchant, offHandEnchant;
+	local pendingSources = { };
 
--- Overwrite the function.
-function C_TransmogCollection.SaveOutfit(name, outfit, enchantOne, enchantTwo, icon)
-	-- Since legion artifacts for example save as 0 instead of their actual visual id,
-	-- we'll have to loop over the entire outfit table, and correct it.
-
-	-- Check if we have an outfit to save.
-	if outfit then
-		-- We are saving an outfit, loop over its slots.
-		for slotID = 0, #outfit do
-			-- Check if we are saving something in this slot.
-			if outfit[slotID] then
-				-- We are saving in this slot, update it.
-				outfit[slotID] = GetSlotVisualSourceID(slotID, LE_TRANSMOG_TYPE_APPEARANCE);
+	for i = 1, #TRANSMOG_SLOTS do
+		local sourceID = self:GetSlotSourceID(TRANSMOG_SLOTS[i].slot, TRANSMOG_SLOTS[i].transmogType);
+		if ( sourceID ~= NO_TRANSMOG_SOURCE_ID ) then
+			if ( TRANSMOG_SLOTS[i].transmogType == LE_TRANSMOG_TYPE_APPEARANCE ) then
+				local slotID = GetInventorySlotInfo(TRANSMOG_SLOTS[i].slot);
+				local isValidSource = C_TransmogCollection.PlayerKnowsSource(sourceID);
+				if ( not isValidSource ) then
+					local isInfoReady, canCollect = C_TransmogCollection.PlayerCanCollectSource(sourceID);
+					if ( isInfoReady ) then
+						if ( canCollect ) then
+							isValidSource = true;
+						end
+					else
+						pendingSources[sourceID] = slotID;
+					end
+				end
+				if ( isValidSource ) then
+					sources[slotID] = sourceID;
+				end
+			elseif ( TRANSMOG_SLOTS[i].transmogType == LE_TRANSMOG_TYPE_ILLUSION ) then
+				if ( TRANSMOG_SLOTS[i].slot == "MAINHANDSLOT" ) then
+					mainHandEnchant = sourceID;
+				else
+					offHandEnchant = sourceID;
+				end
 			end
 		end
 	end
 
-	-- Call the original function.
-	OriginalSaveOutfit(name, outfit, enchantOne, enchantTwo, icon);
+	-- store the state for this save
+	WardrobeOutfitFrame.sources = sources;
+	WardrobeOutfitFrame.mainHandEnchant = mainHandEnchant;
+	WardrobeOutfitFrame.offHandEnchant = offHandEnchant;
+	WardrobeOutfitFrame.pendingSources = pendingSources;
+	WardrobeOutfitFrame.name = name;
+	-- save the dropdown
+	WardrobeOutfitFrame.popupDropDown = self;
+
+	WardrobeOutfitFrame:EvaluateSaveState();
 end
